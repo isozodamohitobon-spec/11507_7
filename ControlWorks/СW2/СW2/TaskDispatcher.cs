5941fb599e9bf5827d5acca16d772127f0a9ee76
isozodamohitobon-spec <isozodamohitobon@gmail.com>
2026-05-23 17:51:53 +0300

    Add files via upload

---
+++ ControlWorks/СW2/СW2/TaskDispatcher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class TaskDispatcher
+{
+    private readonly Semaphore _semaphore;
+    private readonly List<Action> _tasks;
+    
+    public TaskDispatcher(int maxConcurrentTasks = 5)
+    {
+        _semaphore = new Semaphore(maxConcurrentTasks, maxConcurrentTasks);
+        _tasks = new List<Action>();
+    }
+    
+    public void AddTask(Action task)
+    {
+        _tasks.Add(task);
+    }
+    
+    public void ExecuteAll()
+    {
+        List<Task> runningTasks = new List<Task>();
+        
+        foreach (var task in _tasks)
+        {
+            
+            _semaphore.WaitOne();
+            
+            var t = Task.Run(() =>
+            {
+                try
+                {
+                    task?.Invoke();
+                }
+                finally
+                {
+                    _semaphore.Release();
+                }
+            });
+            
+            runningTasks.Add(t);
+        }
+        
+        Task.WaitAll(runningTasks.ToArray());
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        var dispatcher = new TaskDispatcher(maxConcurrentTasks: 5);
+        
+
+        for (int i = 0; i < 50; i++)
+        {
+            int taskId = i;
+            dispatcher.AddTask(() =>
+            {
+                Console.WriteLine($"Task {taskId} started on thread {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(100); 
+                Console.WriteLine($"Task {taskId} completed");
+            });
+        }
+        
+        dispatcher.ExecuteAll();
+        Console.WriteLine("All tasks completed");
+    }
+}
---
+++ ControlWorks/СW2/СW2/TypeFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class TypeFactory
+{
+    public static object CreateAndFill(Type type, Dictionary<string, object> values)
+    {
+        var instance = Activator.CreateInstance(type);
+        
+        var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        
+        foreach (var property in properties)
+        {
+            if (values.TryGetValue(property.Name, out var value))
+            {
+              
+                if (value != null && !property.PropertyType.IsAssignableFrom(value.GetType()))
+                {
+                    try
+                    {
+                        value = Convert.ChangeType(value, property.PropertyType);
+                    }
+                    catch
+                    {
+                        continue; 
+                    }
+                }
+                
+            
+                property.SetValue(instance, value);
+            }
+        }
+
+        return instance;
+    }
+}
